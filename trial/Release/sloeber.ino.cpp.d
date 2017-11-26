7f63cfb12015dc10441923ed29d0f1aff9fb616e
ejeanney <ejeanney@yahoo.fr>
2017-11-26 14:11:53 -0800

    adding blink and .gitignore

---
+++ trial/Release/sloeber.ino.cpp.d
@@ -0,0 +1,37 @@
+sloeber.ino.cpp.o: ../sloeber.ino.cpp \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h \
+ ../AnalogReadSerial.ino
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/root/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h:
+
+../AnalogReadSerial.ino:
--- trial/sloeber.ino.cpp
+++ trial/sloeber.ino.cpp
@@ -2,9 +2,10 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-11-12 17:31:16
+//This file has been generated on 2017-11-26 13:59:00
 
 #include "Arduino.h"
+
 void setup() ;
 void loop() ;
 
