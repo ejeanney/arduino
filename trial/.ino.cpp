1ff0632350f57e8b4e0cc0da30255ab4ed008fa4
ejeanney <ejeanney@yahoo.fr>
2017-11-26 13:24:33 -0800

    initial commit

---
+++ trial/.ino.cpp
@@ -0,0 +1,14 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-11-12 17:31:16
+
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+
+#include "AnalogReadSerial.ino"
+
+#endif
